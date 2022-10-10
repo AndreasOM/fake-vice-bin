502e08012d07439bb4782cb32ea41036186a32a4
Andreas Neukoetter <andreas@omni-mad.com>
2022-10-10 21:12:38 +0100

    : Fix formatting.

--- src/cli_main.rs
+++ src/cli_main.rs
@@ -1,61 +1,58 @@
-
 use fake_vice_bin::FakeViceBin;
 
-
 fn main() -> anyhow::Result<()> {
-	let mut fvb = FakeViceBin::new( "127.0.0.1", 6502 );
+	let mut fvb = FakeViceBin::new("127.0.0.1", 6502);
 
 	let short_delay = std::time::Duration::from_millis(5);
 	let delay = std::time::Duration::from_millis(500);
 	let long_delay = std::time::Duration::from_millis(5000);
 
 	fvb.connect()?;
-	fvb.send_registers_available( 0 )?;
-	
+	fvb.send_registers_available(0)?;
+
 	println!("Reset");
 	std::thread::sleep(delay);
-//	fvb.connect()?;
+	//	fvb.connect()?;
 	fvb.update()?;
 	fvb.send_reset()?;
-//	fvb.disconnect()?;
-	
+	//	fvb.disconnect()?;
+
 	while fvb.is_reset_pending() {
 		std::thread::sleep(delay);
 		fvb.update()?;
 	}
-	
+
 	println!("Exit");
 	std::thread::sleep(delay);
-//	fvb.connect()?;
+	//	fvb.connect()?;
 	fvb.update()?;
 	fvb.send_exit()?;
-//	fvb.disconnect()?;
-	
-	
+	//	fvb.disconnect()?;
+
 	println!("Load");
 	std::thread::sleep(delay);
-//	fvb.connect()?;
+	//	fvb.connect()?;
 	fvb.send_load("main.prg", true)?;
 	fvb.send_exit()?;
 	//fvb.send_ping()?;
-//	fvb.disconnect()?;
+	//	fvb.disconnect()?;
 
 	std::thread::sleep(long_delay);
 
-/*
-	while fvb.is_load_pending() {
-		std::thread::sleep(delay);
-		fvb.update()?;
-	}
-*/	
-//	fvb.connect()?;
-//	loop {
-		fvb.send_advance_instructions( 1000 )?;
-		//fvb.send_ping()?;
-		//fvb.send_exit()?;
-		fvb.update()?;
-		std::thread::sleep(short_delay);
-//	}
+	/*
+		while fvb.is_load_pending() {
+			std::thread::sleep(delay);
+			fvb.update()?;
+		}
+	*/
+	//	fvb.connect()?;
+	//	loop {
+	fvb.send_advance_instructions(1000)?;
+	//fvb.send_ping()?;
+	//fvb.send_exit()?;
+	fvb.update()?;
+	std::thread::sleep(short_delay);
+	//	}
 	loop {
 		fvb.update()?;
 		//fvb.send_ping()?;
--- src/fake_vice_bin.rs
+++ src/fake_vice_bin.rs
@@ -1,86 +1,84 @@
-
-
 use std::collections::HashMap;
 use std::collections::VecDeque;
 use std::io::Read;
 use std::io::Write;
-use std::net::TcpStream;
-use std::str::FromStr;
 use std::net::IpAddr;
 use std::net::SocketAddr;
+use std::net::TcpStream;
+use std::str::FromStr;
 
-#[derive(Debug,Default)]
+#[derive(Debug, Default)]
 pub struct Register {
-	name: String,
+	name:  String,
 	value: u16,
-	size: u8,	// in bits
+	size:  u8, // in bits
 }
 
 impl Register {
-	pub fn set_name( &mut self, name: &str ) {
+	pub fn set_name(&mut self, name: &str) {
 		self.name = name.to_owned();
 	}
-	pub fn set_value( &mut self, value: u16 ) {
+	pub fn set_value(&mut self, value: u16) {
 		self.value = value;
 	}
-	pub fn set_size( &mut self, size: u8 ) {
+	pub fn set_size(&mut self, size: u8) {
 		self.size = size;
 	}
-	pub fn name( &self ) -> &str {
+	pub fn name(&self) -> &str {
 		&self.name
 	}
-	pub fn value( &self ) -> u16 {
+	pub fn value(&self) -> u16 {
 		self.value
 	}
-	pub fn size( &self ) -> u8 {
+	pub fn size(&self) -> u8 {
 		self.size
 	}
 }
 
 #[derive(Debug)]
 pub struct FakeViceBin {
-	socket_addr: SocketAddr,
-	stream: Option<TcpStream>,
+	socket_addr:     SocketAddr,
+	stream:          Option<TcpStream>,
 	response_buffer: VecDeque<u8>,
-	resets_pending: usize,
-	load_pending: bool,
+	resets_pending:  usize,
+	load_pending:    bool,
 	next_request_id: u32,
-	running: bool,
+	running:         bool,
 	program_counter: u16,
-	registers: HashMap< u8, Register >,
+	registers:       HashMap<u8, Register>,
 }
 
 impl FakeViceBin {
-	pub fn new( host: &str, port: u16 ) -> Self {
+	pub fn new(host: &str, port: u16) -> Self {
 		let ip: IpAddr = IpAddr::from_str(host).expect("...");
 		Self {
-			socket_addr: (ip,port).into(),
-			stream: None,
+			socket_addr:     (ip, port).into(),
+			stream:          None,
 			response_buffer: VecDeque::new(),
-			resets_pending: 0,
-			load_pending: false,
+			resets_pending:  0,
+			load_pending:    false,
 			next_request_id: 0,
-			running: true,
+			running:         true,
 			program_counter: 0,
-			registers: HashMap::default(),
+			registers:       HashMap::default(),
 		}
 	}
 
-	pub fn connect( &mut self ) -> anyhow::Result<()> {
-		match TcpStream::connect( self.socket_addr ) {
-			Ok( stream ) => {
+	pub fn connect(&mut self) -> anyhow::Result<()> {
+		match TcpStream::connect(self.socket_addr) {
+			Ok(stream) => {
 				stream.set_nonblocking(true)?;
-				self.stream = Some( stream );
+				self.stream = Some(stream);
 				Ok(())
 			},
-			Err( e ) => {
-				anyhow::bail!("Error connecting to {}: {}", &self.socket_addr, e );
+			Err(e) => {
+				anyhow::bail!("Error connecting to {}: {}", &self.socket_addr, e);
 			},
 		}
 	}
-	pub fn disconnect( &mut self ) -> anyhow::Result<()> {
-		if let Some( stream ) = &mut self.stream.take() {
-			stream.shutdown( std::net::Shutdown::Both )?;
+	pub fn disconnect(&mut self) -> anyhow::Result<()> {
+		if let Some(stream) = &mut self.stream.take() {
+			stream.shutdown(std::net::Shutdown::Both)?;
 			Ok(())
 		} else {
 			Ok(())
@@ -94,70 +92,70 @@ impl FakeViceBin {
 		self.load_pending
 	}
 
-	fn generate_request_id( &mut self ) -> [ u8;4 ] {
+	fn generate_request_id(&mut self) -> [u8; 4] {
 		let id = self.next_request_id;
 		self.next_request_id += 1;
 		[
-			( id >> 0 & 0xff ) as u8,
-			( id >> 1 & 0xff ) as u8,
-			( id >> 2 & 0xff ) as u8,
-			( id >> 3 & 0xff ) as u8,
+			(id >> 0 & 0xff) as u8,
+			(id >> 1 & 0xff) as u8,
+			(id >> 2 & 0xff) as u8,
+			(id >> 3 & 0xff) as u8,
 		]
 	}
-	fn build_command( &mut self, command: u8, mut body: Vec<u8> ) -> Vec< u8 > {
+	fn build_command(&mut self, command: u8, mut body: Vec<u8>) -> Vec<u8> {
 		let l = body.len();
-		let mut buffer = Vec::with_capacity( 12 + l );
-		buffer.push( 0x02 ); // STX
-		buffer.push( 0x02 ); // version
-		// 2-5 body length
-		buffer.push( l as u8 ); // 0x00; // :HACK:
-		buffer.push( 0x00 );
-		buffer.push( 0x00 );
-		buffer.push( 0x00 );
+		let mut buffer = Vec::with_capacity(12 + l);
+		buffer.push(0x02); // STX
+		buffer.push(0x02); // version
+				   // 2-5 body length
+		buffer.push(l as u8); // 0x00; // :HACK:
+		buffer.push(0x00);
+		buffer.push(0x00);
+		buffer.push(0x00);
 		// 6-9 request id -> little endian!
 		let rid = self.generate_request_id();
-		buffer.push( rid[ 0 ] );
-		buffer.push( rid[ 1 ] );
-		buffer.push( rid[ 2 ] );
-		buffer.push( rid[ 3 ] );
+		buffer.push(rid[0]);
+		buffer.push(rid[1]);
+		buffer.push(rid[2]);
+		buffer.push(rid[3]);
 		// 10 command id
-		buffer.push( command );
+		buffer.push(command);
 		// 11 command body
-		buffer.append( &mut body );
+		buffer.append(&mut body);
 
 		buffer
 	}
 
-	fn handle_response( &mut self ) -> anyhow::Result<()> {
+	fn handle_response(&mut self) -> anyhow::Result<()> {
 		if self.response_buffer.len() >= 12 {
 			let header_buffer = self.response_buffer.drain(0..12).collect::<Vec<_>>();
 			let buffer = &header_buffer;
 			for b in buffer.iter() {
-				print!("{:#02x} ", b );
+				print!("{:#02x} ", b);
 			}
 			println!("");
 			// parse update
-			let stx = buffer[ 0 ];
+			let stx = buffer[0];
 			if stx != 0x02 {
-				anyhow::bail!("Response started with {}", stx );
+				anyhow::bail!("Response started with {}", stx);
 			}
-			let version = buffer[ 1 ];
+			let version = buffer[1];
 			if version != 0x02 {
-				anyhow::bail!("Version {} not supported", version );
+				anyhow::bail!("Version {} not supported", version);
 			}
-			let len = [ buffer[ 5 ], buffer[ 4 ], buffer[ 3 ], buffer[ 2 ] ];
+			let len = [buffer[5], buffer[4], buffer[3], buffer[2]];
 			let mut body_len = 0;
 			for b in len.iter() {
 				body_len <<= 8;
 				body_len |= *b as usize;
 			}
 			//println!("Body Length: {:?} -> {}", len, body_len);
-			let response_type = buffer[ 6 ];
+			let response_type = buffer[6];
 			//println!("Response type: {:#02x}", response_type);
-			let error_code = buffer[ 7 ];
+			let error_code = buffer[7];
 			//println!("Error code: {:#02x}", error_code);
 
-			let id = [ buffer[ 11 ], buffer[ 10 ], buffer[ 9 ], buffer[ 8 ] ];
+			let id = [buffer[11], buffer[10], buffer[9], buffer[8]];
 			let mut request_id = 0;
 			for b in id.iter() {
 				request_id <<= 8;
@@ -173,8 +171,9 @@ impl FakeViceBin {
 			let body_buffer = self.response_buffer.drain(0..body_len).collect::<Vec<_>>();
 			let buffer = &body_buffer;
 			match response_type {
-				0x31 => {	// registers get
-					let c = [ buffer[ 1 ], buffer[ 0 ] ];
+				0x31 => {
+					// registers get
+					let c = [buffer[1], buffer[0]];
 					let mut count = 0;
 					for b in c.iter() {
 						count <<= 8;
@@ -183,18 +182,30 @@ impl FakeViceBin {
 					//println!("Count {}", count);
 					let mut entry_start = 2;
 					for e in 0..count {
-						let size = buffer[ entry_start ];
-						let id = buffer[ entry_start+1 ];
-						let value = (( buffer[ entry_start+3 ] as u16 ) << 8 ) | ( buffer[ entry_start+2 ] as u16 );
+						let size = buffer[entry_start];
+						let id = buffer[entry_start + 1];
+						let value = ((buffer[entry_start + 3] as u16) << 8)
+							| (buffer[entry_start + 2] as u16);
 
-						let r = self.registers.entry( id ).or_insert_with(|| Register::default());
-						println!("{:#02} | {:#04x} {:#04x} {:#06x} | {}", e, size, id, value, r.name());
-						r.set_value( value );
+						let r = self
+							.registers
+							.entry(id)
+							.or_insert_with(|| Register::default());
+						println!(
+							"{:#02} | {:#04x} {:#04x} {:#06x} | {}",
+							e,
+							size,
+							id,
+							value,
+							r.name()
+						);
+						r.set_value(value);
 						entry_start += 4;
 					}
 				},
-				0x62 => {	// stopped
-					let c = [ buffer[ 1 ], buffer[ 0 ] ];
+				0x62 => {
+					// stopped
+					let c = [buffer[1], buffer[0]];
 					let mut pc = 0;
 					for b in c.iter() {
 						pc <<= 8;
@@ -204,8 +215,9 @@ impl FakeViceBin {
 					self.program_counter = pc;
 					//println!("stopped PC {:#06x}", pc);
 				},
-				0x63 => {	// resumed
-					let c = [ buffer[ 1 ], buffer[ 0 ] ];
+				0x63 => {
+					// resumed
+					let c = [buffer[1], buffer[0]];
 					let mut pc = 0;
 					for b in c.iter() {
 						pc <<= 8;
@@ -215,70 +227,75 @@ impl FakeViceBin {
 					self.program_counter = pc;
 					//println!("resumed PC {:#06x}", pc);
 				},
-				0x71 => {	// advance instructions
-
+				0x71 => { // advance instructions
 				},
-				0x81 => {	// ping
-
+				0x81 => { // ping
 				},
-				0x83 => {	// registers available
+				0x83 => {
+					// registers available
 					println!("Body for 0x83 - registers available");
 					for b in buffer.iter() {
-						print!("{:#02x} ", b );
+						print!("{:#02x} ", b);
 					}
 					println!("");
 
 					/*
-byte 0-1: The count of the array items
-byte 2+: An array with items of structure:
+					byte 0-1: The count of the array items
+					byte 2+: An array with items of structure:
 
-byte 0: Size of the item, excluding this byte
-byte 1: ID of the register
-byte 2: Size of the register in bits
-byte 3: Length of name
-byte 4+: Name 
-    				*/
+					byte 0: Size of the item, excluding this byte
+					byte 1: ID of the register
+					byte 2: Size of the register in bits
+					byte 3: Length of name
+					byte 4+: Name
+										*/
 
-    				let count = ( buffer[ 1 ] as u16 ) << 8 | ( buffer[ 0 ] as u16 );
-    				println!("Entry count {}", count);
+					let count = (buffer[1] as u16) << 8 | (buffer[0] as u16);
+					println!("Entry count {}", count);
 
-    				let mut entry_start = 2;
-    				for e in 0..count {
-						let size = buffer[ entry_start ] as usize;
-						let id = buffer[ entry_start+1 ];
-						let r_size = buffer[ entry_start+2 ];
-						let len = buffer[ entry_start+3 ] as usize;
+					let mut entry_start = 2;
+					for e in 0..count {
+						let size = buffer[entry_start] as usize;
+						let id = buffer[entry_start + 1];
+						let r_size = buffer[entry_start + 2];
+						let len = buffer[entry_start + 3] as usize;
 						let mut name = Vec::new();
 						for i in 0..=len {
-							name.push( buffer[ entry_start +3 + i])
+							name.push(buffer[entry_start + 3 + i])
 						}
 
-						let name = std::str::from_utf8( &name )?;
+						let name = std::str::from_utf8(&name)?;
 
-						println!("{:#02} | {:#04x} {:#04x} {:#04x} -> {}", e, size, id, r_size, name);
-						let r = self.registers.entry( id ).or_insert_with(|| Register::default());
-						r.set_name( name );
-						r.set_size( r_size );
+						println!(
+							"{:#02} | {:#04x} {:#04x} {:#04x} -> {}",
+							e, size, id, r_size, name
+						);
+						let r = self
+							.registers
+							.entry(id)
+							.or_insert_with(|| Register::default());
+						r.set_name(name);
+						r.set_size(r_size);
 						entry_start += size + 1;
-
-    				}
+					}
 				},
-				0xaa => {	// exit
-
+				0xaa => { // exit
 				},
-				0xcc => {	// reset
+				0xcc => {
+					// reset
 					println!("Handled reset");
 					self.resets_pending -= 1;
 				},
-				o => {
-					match error_code {
-						0x80 => {
-							println!("Invalid command length for {:#010x}", request_id);
-						},
-						ec => {
-							println!("Unhandled response type {:#04x} (error code: {:#04x})", o, ec);
-						},
-					}
+				o => match error_code {
+					0x80 => {
+						println!("Invalid command length for {:#010x}", request_id);
+					},
+					ec => {
+						println!(
+							"Unhandled response type {:#04x} (error code: {:#04x})",
+							o, ec
+						);
+					},
 				},
 			}
 		}
@@ -286,40 +303,39 @@ byte 4+: Name
 		Ok(())
 	}
 
-	pub fn update( &mut self ) -> anyhow::Result<()> {
+	pub fn update(&mut self) -> anyhow::Result<()> {
 		println!("{} {:?}", &self.resets_pending, self.load_pending);
-		if let Some( stream ) = &mut self.stream {
-			let mut buf = [0;1];
-//			let mut buf = Vec::with_capacity( 100 );
+		if let Some(stream) = &mut self.stream {
+			let mut buf = [0; 1];
+			//			let mut buf = Vec::with_capacity( 100 );
 			// match stream.read_to_end( &mut buf ) {
 			loop {
-				let _size = match stream.read( &mut buf ) {				
-					Ok( size ) => {
+				let _size = match stream.read(&mut buf) {
+					Ok(size) => {
 						//println!("Read {} bytes from stream", size);
 						size
 					},
-					Err( ref e ) => {
+					Err(ref e) => {
 						match e.kind() {
 							std::io::ErrorKind::WouldBlock => {
 								//println!("No updates");
 								break;
 							},
 							e => {
-								anyhow::bail!("Error reading from to {}: {}", &self.socket_addr, e );
+								anyhow::bail!("Error reading from to {}: {}", &self.socket_addr, e);
 							},
 						}
 					},
 				};
 
 				for b in buf.iter() {
-					self.response_buffer.push_back( *b );
+					self.response_buffer.push_back(*b);
 				}
-
 			}
 			// println!("Read {} bytes in update", self.response_buffer.len() );
 			while self.response_buffer.len() >= 12 {
 				self.handle_response()?;
-			} 
+			}
 
 			Ok(())
 		} else {
@@ -327,74 +343,74 @@ byte 4+: Name
 		}
 	}
 
-	pub fn send_ping( &mut self ) -> anyhow::Result<()> {
-		if let Some( stream ) = &mut self.stream {
+	pub fn send_ping(&mut self) -> anyhow::Result<()> {
+		if let Some(stream) = &mut self.stream {
 			// :TODO: read pending -> read_to_end
 
 			let mut buf = Vec::new();
 
-			buf.push( 0x02 ); // STX
-			buf.push( 0x02 ); // version
-			// 2-5 body length
-			buf.push( 0x00 );
-			buf.push( 0x00 );
-			buf.push( 0x00 );
-			buf.push( 0x00 );
+			buf.push(0x02); // STX
+			buf.push(0x02); // version
+				// 2-5 body length
+			buf.push(0x00);
+			buf.push(0x00);
+			buf.push(0x00);
+			buf.push(0x00);
 			// 6-9 request id -> little endian!
-			buf.push( 0x00 );
-			buf.push( 0x00 );
-			buf.push( 0x00 );
-			buf.push( 0x00 );
+			buf.push(0x00);
+			buf.push(0x00);
+			buf.push(0x00);
+			buf.push(0x00);
 			// 10 command id
-			buf.push( 0x81 ); // 0x81 -> ping
-			// 11 command body
+			buf.push(0x81); // 0x81 -> ping
+				// 11 command body
 
-			stream.write( buf.as_slice() )?;
+			stream.write(buf.as_slice())?;
 			Ok(())
 		} else {
 			anyhow::bail!("No stream to send ping");
 		}
 	}
 
-	pub fn send_exit( &mut self ) -> anyhow::Result<()> {
-		if let Some( stream ) = &mut self.stream {
+	pub fn send_exit(&mut self) -> anyhow::Result<()> {
+		if let Some(stream) = &mut self.stream {
 			// :TODO: read pending -> read_to_end
 
 			let mut buf = Vec::new();
 
-			buf.push( 0x02 ); // STX
-			buf.push( 0x02 ); // version
-			// 2-5 body length
-			buf.push( 0x00 );
-			buf.push( 0x00 );
-			buf.push( 0x00 );
-			buf.push( 0x00 );
+			buf.push(0x02); // STX
+			buf.push(0x02); // version
+				// 2-5 body length
+			buf.push(0x00);
+			buf.push(0x00);
+			buf.push(0x00);
+			buf.push(0x00);
 			// 6-9 request id -> little endian!
-			buf.push( 0x00 );
-			buf.push( 0x00 );
-			buf.push( 0x00 );
-			buf.push( 0x00 );
+			buf.push(0x00);
+			buf.push(0x00);
+			buf.push(0x00);
+			buf.push(0x00);
 			// 10 command id
-			buf.push( 0xaa ); // 0xaa -> exit
-			// 11 command body
+			buf.push(0xaa); // 0xaa -> exit
+				// 11 command body
 
-			stream.write( buf.as_slice() )?;
+			stream.write(buf.as_slice())?;
 			Ok(())
 		} else {
 			anyhow::bail!("No stream to send exit");
 		}
 	}
 
-	pub fn send_reset( &mut self ) -> anyhow::Result<()> {
+	pub fn send_reset(&mut self) -> anyhow::Result<()> {
 		if self.stream.is_some() {
 			// :TODO: read pending -> read_to_end
 
 			let mut body = Vec::new();
-			body.push( 0x01 ); // 0x01 -> hard reset
+			body.push(0x01); // 0x01 -> hard reset
 
-			let buf = self.build_command( 0xcc, body);
-			if let Some( stream ) = &mut self.stream {
-				stream.write( buf.as_slice() )?;
+			let buf = self.build_command(0xcc, body);
+			if let Some(stream) = &mut self.stream {
+				stream.write(buf.as_slice())?;
 				self.resets_pending += 1;
 			}
 			Ok(())
@@ -402,7 +418,7 @@ byte 4+: Name
 			anyhow::bail!("No stream to send reset");
 		}
 	}
-	pub fn send_load( &mut self, filename: &str, autostart: bool ) -> anyhow::Result<()> {
+	pub fn send_load(&mut self, filename: &str, autostart: bool) -> anyhow::Result<()> {
 		if self.stream.is_some() {
 			let filename_bytes: &[u8] = filename.as_bytes();
 			// :TODO: ascii cleanup/check
@@ -411,21 +427,21 @@ byte 4+: Name
 
 			let mut body = Vec::new();
 			if autostart {
-				body.push( 0x01 ); // autostart
+				body.push(0x01); // autostart
 			} else {
-				body.push( 0x00 ); // no autostart
+				body.push(0x00); // no autostart
 			}
 			// file index of disk image
-			body.push( 0x00 );
-			body.push( 0x00 );
-			body.push( filename_len as u8);
+			body.push(0x00);
+			body.push(0x00);
+			body.push(filename_len as u8);
 			for b in filename_bytes.iter() {
-				body.push( *b );
+				body.push(*b);
 			}
 
-			let buf = self.build_command( 0xdd, body);
-			if let Some( stream ) = &mut self.stream {
-				stream.write( buf.as_slice() )?;
+			let buf = self.build_command(0xdd, body);
+			if let Some(stream) = &mut self.stream {
+				stream.write(buf.as_slice())?;
 				self.load_pending = true;
 			}
 			Ok(())
@@ -433,30 +449,30 @@ byte 4+: Name
 			anyhow::bail!("No stream to send load");
 		}
 	}
-	pub fn send_registers_available( &mut self, memspace: u8 ) -> anyhow::Result<()> {
+	pub fn send_registers_available(&mut self, memspace: u8) -> anyhow::Result<()> {
 		if self.stream.is_some() {
 			let mut body = Vec::new();
-				body.push( memspace );
+			body.push(memspace);
 
-			let buf = self.build_command( 0x83, body);
-			if let Some( stream ) = &mut self.stream {
-				stream.write( buf.as_slice() )?;
+			let buf = self.build_command(0x83, body);
+			if let Some(stream) = &mut self.stream {
+				stream.write(buf.as_slice())?;
 			}
 			Ok(())
 		} else {
 			anyhow::bail!("No stream to send registers available");
 		}
 	}
-	pub fn send_advance_instructions( &mut self, count: u16 ) -> anyhow::Result<()> {
+	pub fn send_advance_instructions(&mut self, count: u16) -> anyhow::Result<()> {
 		if self.stream.is_some() {
 			let mut body = Vec::new();
-			body.push( 0 );	// do not step over subroutines
-			body.push( ( ( count >> 0  ) & 0xff ) as u8 );
-			body.push( ( ( count >> 1  ) & 0xff ) as u8 );
+			body.push(0); // do not step over subroutines
+			body.push(((count >> 0) & 0xff) as u8);
+			body.push(((count >> 1) & 0xff) as u8);
 
-			let buf = self.build_command( 0x71, body);
-			if let Some( stream ) = &mut self.stream {
-				stream.write( buf.as_slice() )?;
+			let buf = self.build_command(0x71, body);
+			if let Some(stream) = &mut self.stream {
+				stream.write(buf.as_slice())?;
 			}
 			Ok(())
 		} else {
